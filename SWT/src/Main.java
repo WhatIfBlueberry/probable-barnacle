597850de76a62e11208d4897293819c73b16b143
WhatIfBlueberry <Dylan-Pascal@web.de>
2022-03-16 14:58:21 +0100

    got rid of no longer useful files and updated Main to be a cool SWT Widget

--- SWT/src/Main.java
+++ SWT/src/Main.java
@@ -1,9 +1,39 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("But i can be so much more!");
+		
+		Display display = new Display();
+		Shell shell = new Shell();
+		
+		shell.setLayout(new GridLayout());
+		shell.setText("First Program of its kind, long!");
+		
+		Text text = new Text(shell, SWT.SHADOW_ETCHED_OUT);
+		Button button = new Button(shell, SWT.PUSH);
+		button.setText("Foo");
+		button.pack();
+		
+		
+		button.addSelectionListener(SelectionListener.widgetSelectedAdapter(e-> System.out.println("Pressed")));
+		
+		text.setText("Hello oh you wonderfull world!");
+		text.pack();
+		
+		shell.pack();
+		shell.open();
+		
+		while(!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+
 	}
 
 }
